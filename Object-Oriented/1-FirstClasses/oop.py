e03339f0504bcf1c3837f87c8c2296d9e9077e95
okorokovnikita <57351674+okorokovnikita@users.noreply.github.com>
2019-12-08 19:11:13 +0500

    Update oop.py

--- Object-Oriented/1-FirstClasses/oop.py
+++ Object-Oriented/1-FirstClasses/oop.py
@@ -2,10 +2,10 @@
 class Employee:
 
     def __init__(self, first, last, pay): #here we initialize the variables of the class object
-        self.first = first
-        self.last = last
-        self.email = first + '.' + last + '@email.com'
-        self.pay = pay
+        self.first = first #this is attribute of the class
+        self.last = last #this is attribute of the class
+        self.email = first + '.' + last + '@email.com' #this is attribute of the class
+        self.pay = pay #this is attribute of the class
 
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
